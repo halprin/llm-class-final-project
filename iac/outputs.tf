f59d126dacafa58d7d74a6fb71f87ba55c4ac332
halprin <halprin@users.noreply.github.com>
2025-09-26 20:10:42 -0600

    Force new update after push

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -3,3 +3,13 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.app.repository_url
   sensitive   = true
 }
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.cluster.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.app.name
+}
