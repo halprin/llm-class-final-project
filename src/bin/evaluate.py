f9479b47a77cb8958508f9cc73b113a566ebedce
halprin <halprin@users.noreply.github.com>
2025-09-27 13:42:08 -0600

    Basic evaluator

---
+++ src/bin/evaluate.py
@@ -0,0 +1,34 @@
+from evaluate import load
+
+from llm import Llm
+from rag.database import Database
+
+
+class Evaluator:
+    def __init__(self, model: Llm, dataset, database: Database):
+        self._model = model
+        self._database = database
+        self._dataset = dataset
+        self._metric_type = "rouge"
+        self._metric = load(self._metric_type)
+
+    def evaluate(self):
+        expecteds = []
+        actuals = []
+
+        for datapoint in self._dataset:
+            expecteds.append(datapoint["expected"])
+
+            prompt = datapoint["prompt"]
+            retrieved_docs = self._database.retrieve_documents(prompt)
+
+            full_response = ""
+            response_stream = self._model.stream(prompt, retrieved_docs)
+            for chunk in response_stream:
+                full_response += chunk
+
+            actuals.append(full_response)
+
+        metric = self._metric.compute(predictions=actuals, references=expecteds)
+
+        return metric[self._metric_type]
